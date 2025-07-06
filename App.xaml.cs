0201c2e83fac09db98ab3f6ecc7afc53ffae4679
CubeIR <cube.master.ir@outlook.com>
2025-07-07 01:26:54 +0330

    Update App.xaml.cs
    Block multiple instances, bring to foreground

--- App.xaml.cs
+++ App.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -11,6 +12,8 @@ using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using Microsoft.UI.Xaml.Shapes;
+using Microsoft.Windows.AppNotifications;
+using Microsoft.Windows.AppNotifications.Builder;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
 using Windows.Foundation;
@@ -26,6 +29,7 @@ namespace Vanilla_RTX_Tuner_WinUI;
 public partial class App : Application
 {
     private Window? _window;
+    private static Mutex _mutex = null;
 
     /// <summary>
     /// Initializes the singleton application object.  This is the first line of authored code
@@ -42,7 +46,58 @@ public partial class App : Application
     /// <param name="args">Details about the launch request and process.</param>
     protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
     {
+        bool isNewInstance;
+        _mutex = new Mutex(true, "VanillaRTXTuner_SingleInstance", out isNewInstance);
+
+        if (!isNewInstance)
+        {
+            BringExistingWindowToFront();
+
+            // then xit without creating any new windows
+            Exit();
+            return;
+        }
+
+        // Continue with app initialization only if this is a new instance
         _window = new MainWindow();
         _window.Activate();
     }
+
+    // Clean up mutex when app exits
+    ~App()
+    {
+        _mutex?.ReleaseMutex();
+        _mutex?.Dispose();
+    }
+
+    public static void CleanupMutex()
+    {
+        _mutex?.ReleaseMutex();
+        _mutex?.Dispose();
+    }
+
+    private void BringExistingWindowToFront()
+    {
+        // Find the existing window process
+        var processes = System.Diagnostics.Process.GetProcessesByName("Vanilla RTX Tuner WinUI");
+        foreach (var process in processes)
+        {
+            if (process.Id != Environment.ProcessId)
+            {
+                // Bring the existing window to foreground
+                ShowWindow(process.MainWindowHandle, SW_RESTORE);
+                SetForegroundWindow(process.MainWindowHandle);
+                break;
+            }
+        }
+    }
+
+    // Windows API
+    [System.Runtime.InteropServices.DllImport("user32.dll")]
+    private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+    [System.Runtime.InteropServices.DllImport("user32.dll")]
+    private static extern bool SetForegroundWindow(IntPtr hWnd);
+
+    private const int SW_RESTORE = 9;
 }
