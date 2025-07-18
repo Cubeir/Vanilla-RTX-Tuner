0163fa9f6d13f39c7f78781fad3191f6a9bdc370
CubeIR <cube.master.ir@outlook.com>
2025-07-18 07:12:53 +0330

    Exclude ❤️ button

--- WindowControlsManager.cs
+++ WindowControlsManager.cs
@@ -17,6 +17,7 @@ public class WindowControlsManager
     private static readonly HashSet<string> _globalExclusions = new()
         {
             "HelpButton",
+            "DonateButton",
             "SidebarLog",
             "SidelogProgressBar"
         };
