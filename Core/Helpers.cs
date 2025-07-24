b0850262ad158c67dacef1e2f23768af5df5d90a
CubeIR <cube.master.ir@outlook.com>
2025-07-24 13:45:02 +0330

    Remove GetConfig & Simplify

--- Core/Helpers.cs
+++ Core/Helpers.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.IO.Compression;
@@ -9,8 +8,6 @@ using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using ImageMagick;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 using Windows.Storage.Pickers;
 using static Vanilla_RTX_Tuner_WinUI.MainWindow;
 
@@ -18,106 +15,102 @@ namespace Vanilla_RTX_Tuner_WinUI.Core;
 
 public static class Helpers
 {
-    // Direct TGA handling for raw PBR data - avoids unwanted colorspace conversions with better performance
     public static Bitmap ReadImage(string imagePath, bool maxOpacity = false)
     {
         try
         {
-            using (var sourceImage = new MagickImage(imagePath))
+            using var sourceImage = new MagickImage(imagePath);
+            var width = (int)sourceImage.Width;
+            var height = (int)sourceImage.Height;
+
+
+            var bitmap = new Bitmap(width, height, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+
+            using (var sourcePixels = sourceImage.GetPixels())
             {
-                var width = (int)sourceImage.Width;
-                var height = (int)sourceImage.Height;
 
-
-                var bitmap = new Bitmap(width, height, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
-
-                using (var sourcePixels = sourceImage.GetPixels())
+                for (var y = 0; y < height; y++)
                 {
-
-                    for (var y = 0; y < height; y++)
+                    for (var x = 0; x < width; x++)
                     {
-                        for (var x = 0; x < width; x++)
+                        var pixelData = sourcePixels.GetPixel(x, y);
+
+                        byte r, g, b, a;
+
+                        var hasAlpha = sourceImage.HasAlpha || sourceImage.ColorType == ColorType.GrayscaleAlpha || sourceImage.ColorType == ColorType.TrueColorAlpha;
+
+                        if (sourceImage.ColorType == ColorType.Grayscale)
                         {
-                            var pixelData = sourcePixels.GetPixel(x, y);
+                            var gray = (byte)(pixelData[0] >> 8);
+                            r = g = b = gray;
+                            a = 255;
+                        }
+                        else if (sourceImage.ColorType == ColorType.GrayscaleAlpha)
+                        {
+                            var gray = (byte)(pixelData[0] >> 8);
+                            r = g = b = gray;
+                            var originalAlpha = (byte)(pixelData[1] >> 8);
+                            a = maxOpacity ? (byte)255 : originalAlpha;
+                        }
+                        else if (sourceImage.ColorType == ColorType.TrueColor)
+                        {
+                            r = (byte)(pixelData[0] >> 8);
+                            g = (byte)(pixelData[1] >> 8);
+                            b = (byte)(pixelData[2] >> 8);
+                            a = 255;
+                        }
+                        else if (sourceImage.ColorType == ColorType.TrueColorAlpha)
+                        {
+                            r = (byte)(pixelData[0] >> 8);
+                            g = (byte)(pixelData[1] >> 8);
+                            b = (byte)(pixelData[2] >> 8);
+                            var originalAlpha = (byte)(pixelData[3] >> 8);
+                            a = maxOpacity ? (byte)255 : originalAlpha;
+                        }
+                        else if (sourceImage.ColorType == ColorType.Palette)
+                        {
+                            r = (byte)(pixelData[0] >> 8);
+                            g = (byte)(pixelData[1] >> 8);
+                            b = (byte)(pixelData[2] >> 8);
 
-                            byte r, g, b, a;
-
-                            var hasAlpha = sourceImage.HasAlpha || sourceImage.ColorType == ColorType.GrayscaleAlpha || sourceImage.ColorType == ColorType.TrueColorAlpha;
-
-                            if (sourceImage.ColorType == ColorType.Grayscale)
+                            if (hasAlpha && sourceImage.ChannelCount > 3)
                             {
-                                var gray = (byte)(pixelData[0] >> 8);
-                                r = g = b = gray;
-                                a = 255;
-                            }
-                            else if (sourceImage.ColorType == ColorType.GrayscaleAlpha)
-                            {
-                                var gray = (byte)(pixelData[0] >> 8);
-                                r = g = b = gray;
-                                var originalAlpha = (byte)(pixelData[1] >> 8);
-                                a = maxOpacity ? (byte)255 : originalAlpha;
-                            }
-                            else if (sourceImage.ColorType == ColorType.TrueColor)
-                            {
-                                r = (byte)(pixelData[0] >> 8);
-                                g = (byte)(pixelData[1] >> 8);
-                                b = (byte)(pixelData[2] >> 8);
-                                a = 255;
-                            }
-                            else if (sourceImage.ColorType == ColorType.TrueColorAlpha)
-                            {
-                                r = (byte)(pixelData[0] >> 8);
-                                g = (byte)(pixelData[1] >> 8);
-                                b = (byte)(pixelData[2] >> 8);
                                 var originalAlpha = (byte)(pixelData[3] >> 8);
                                 a = maxOpacity ? (byte)255 : originalAlpha;
                             }
-                            else if (sourceImage.ColorType == ColorType.Palette)
+                            else
                             {
-                                r = (byte)(pixelData[0] >> 8);
-                                g = (byte)(pixelData[1] >> 8);
-                                b = (byte)(pixelData[2] >> 8);
+                                a = 255;
+                            }
+                        }
+                        else
+                        {
+                            var channels = (int)sourceImage.ChannelCount;
 
-                                if (hasAlpha && sourceImage.ChannelCount > 3)
-                                {
-                                    var originalAlpha = (byte)(pixelData[3] >> 8);
-                                    a = maxOpacity ? (byte)255 : originalAlpha;
-                                }
-                                else
-                                {
-                                    a = 255;
-                                }
+                            r = channels > 0 ? (byte)(pixelData[0] >> 8) : (byte)0;
+                            g = channels > 1 ? (byte)(pixelData[1] >> 8) : r;
+                            b = channels > 2 ? (byte)(pixelData[2] >> 8) : r;
+
+                            if (hasAlpha && channels > 3)
+                            {
+                                var originalAlpha = (byte)(pixelData[3] >> 8);
+                                a = maxOpacity ? (byte)255 : originalAlpha;
                             }
                             else
                             {
-                                var channels = (int)sourceImage.ChannelCount;
-
-                                r = channels > 0 ? (byte)(pixelData[0] >> 8) : (byte)0;
-                                g = channels > 1 ? (byte)(pixelData[1] >> 8) : r;
-                                b = channels > 2 ? (byte)(pixelData[2] >> 8) : r;
-
-                                if (hasAlpha && channels > 3)
-                                {
-                                    var originalAlpha = (byte)(pixelData[3] >> 8);
-                                    a = maxOpacity ? (byte)255 : originalAlpha;
-                                }
-                                else
-                                {
-                                    a = 255;
-                                }
+                                a = 255;
                             }
-                            var pixelColor = Color.FromArgb(a, r, g, b);
-                            bitmap.SetPixel(x, y, pixelColor);
                         }
+                        var pixelColor = Color.FromArgb(a, r, g, b);
+                        bitmap.SetPixel(x, y, pixelColor);
                     }
                 }
-
-                return bitmap;
             }
+
+            return bitmap;
         }
-        catch (Exception ex)
+        catch (Exception)
         {
-            // Log($"Error reading image {imagePath}: {ex.Message}");
             var errorBitmap = new Bitmap(512, 512, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
             using (var g = Graphics.FromImage(errorBitmap))
             {
@@ -139,34 +132,32 @@ public static class Helpers
             var height = bitmap.Height;
 
             // Write TGA file format manually for absolute control
-            using (var fs = new FileStream(outputPath, FileMode.Create, FileAccess.Write))
-            using (var writer = new BinaryWriter(fs))
+            using var fs = new FileStream(outputPath, FileMode.Create, FileAccess.Write);
+            using var writer = new BinaryWriter(fs);
+            // TGA
+            writer.Write((byte)0);    // ID Length
+            writer.Write((byte)0);    // Color Map Type (0 = no color map)
+            writer.Write((byte)2);    // Image Type (2 = uncompressed RGB)
+            writer.Write((ushort)0);  // Color Map First Entry Index
+            writer.Write((ushort)0);  // Color Map Length
+            writer.Write((byte)0);    // Color Map Entry Size
+            writer.Write((ushort)0);  // X-origin
+            writer.Write((ushort)0);  // Y-origin
+            writer.Write((ushort)width);  // Width
+            writer.Write((ushort)height); // Height
+            writer.Write((byte)32);       // Pixel Depth (32-bit RGBA)
+            writer.Write((byte)8);        // Image Descriptor (default origin, 8-bit alpha)
+
+            for (var y = height - 1; y >= 0; y--) // TGA is bottom-up by default
             {
-                // TGA
-                writer.Write((byte)0);    // ID Length
-                writer.Write((byte)0);    // Color Map Type (0 = no color map)
-                writer.Write((byte)2);    // Image Type (2 = uncompressed RGB)
-                writer.Write((ushort)0);  // Color Map First Entry Index
-                writer.Write((ushort)0);  // Color Map Length
-                writer.Write((byte)0);    // Color Map Entry Size
-                writer.Write((ushort)0);  // X-origin
-                writer.Write((ushort)0);  // Y-origin
-                writer.Write((ushort)width);  // Width
-                writer.Write((ushort)height); // Height
-                writer.Write((byte)32);       // Pixel Depth (32-bit RGBA)
-                writer.Write((byte)8);        // Image Descriptor (default origin, 8-bit alpha)
-
-                for (var y = height - 1; y >= 0; y--) // TGA is bottom-up by default
+                for (var x = 0; x < width; x++)
                 {
-                    for (var x = 0; x < width; x++)
-                    {
-                        var pixel = bitmap.GetPixel(x, y);
+                    var pixel = bitmap.GetPixel(x, y);
 
-                        writer.Write(pixel.B);
-                        writer.Write(pixel.G);
-                        writer.Write(pixel.R);
-                        writer.Write(pixel.A);
-                    }
+                    writer.Write(pixel.B);
+                    writer.Write(pixel.G);
+                    writer.Write(pixel.R);
+                    writer.Write(pixel.A);
                 }
             }
         }
@@ -176,39 +167,6 @@ public static class Helpers
             throw;
         }
     }
-
-
-    // TODO: Replace this with uwp appdata? (safer and you're already using packaged app so..)
-    // Thing is good for should-be-exposed constants, while uwp storage is better for persistant per-user generated data
-    // Nevertheless the method could become more robust, it is too loose right now.
-    public static T GetConfig<T>(string paramName)
-    {
-        var jsonFilePath = Path.Combine(AppContext.BaseDirectory, "Assets", "config.json");
-        var jsonContent = File.ReadAllText(jsonFilePath);
-
-        var jsonObject = JsonConvert.DeserializeObject<JObject>(jsonContent);
-
-        foreach (var group in jsonObject)
-        {
-            if (group.Value is JObject settingsGroup)
-            {
-                JToken parameterValue;
-                if (settingsGroup.TryGetValue(paramName, out parameterValue))
-                {
-                    try
-                    {
-                        return parameterValue.ToObject<T>();
-                    }
-                    catch (Exception ex)
-                    {
-                        Log($"Error returning parameter value for '{paramName}' to type {typeof(T)}: {ex.Message}", LogLevel.Error);
-                    }
-                }
-            }
-        }
-        return default;
-    }
-
 
 
     private static readonly HttpClient SharedHttpClient = new();
@@ -262,7 +220,7 @@ public static class Helpers
                 if (fileName.Length > 128) fileName = fileName.Substring(0, 128);
 
                 // === LOCATION RESOLUTION WITH FALLBACK === Temp dir, user dl folder, app's data dir, app's dir, desktop as last resort
-                string savingLocation = null;
+                string? savingLocation = null;
                 var fallbackLocations = new Func<string>[]
                 {
                     () => Path.Combine(Path.GetTempPath(), "vanilla_rtx_tuner_cache", fileName),
